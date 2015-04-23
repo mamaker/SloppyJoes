83ff158d1cbbcb788f40a49099005c673d8ea9a3
mamaker <madhuvasudevan@hotmail.com>
2015-04-23 00:03:41 -0500

    Done2!

--- SloppyJoes/Form1.cs
+++ SloppyJoes/Form1.cs
@@ -12,9 +12,29 @@ namespace SloppyJoes
 {
     public partial class Form1 : Form
     {
+        MenuMaker menu = new MenuMaker() { Randomizer = new Random() };
+
         public Form1()
         {
             InitializeComponent();
+            ChangeMenu();
+           
+        }
+
+        public void ChangeMenu()
+        {
+            label1.Text = menu.GetMenuItem();
+            label2.Text = menu.GetMenuItem();
+            label3.Text = menu.GetMenuItem();
+            label4.Text = menu.GetMenuItem();
+            label5.Text = menu.GetMenuItem();
+            label6.Text = menu.GetMenuItem();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChangeMenu();
         }
     }
 }
